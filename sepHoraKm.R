292b920576960ad91c36a06461ee9cab36f764a6
otluiz <olto@ecomp.poli.br>
2017-03-22 19:03:45 -0300

    construção da matriz de mortos

---
+++ sepHoraKm.R
@@ -0,0 +1,205 @@
+#############################################
+setwd("~/workspace/R/")
+
+# free memory
+rm(list = ls())
+gc()
+
+################################################################################
+############################             #######################################
+###########################     BR 101      ####################################
+############################             #######################################
+################################################################################
+
+## calcula quantidade de mortos o atributo Gravidade------------------------------------------------
+df <- read.csv("./data/LimpoCheiroso.csv") ## carrega o data frame
+
+#### limpando dados
+df$Binario <- NULL
+df$Gravidade <- NULL
+df$DiaDaSemana <- NULL
+df$Municipio <- NULL
+df$TotFerVivos <- NULL
+df$TipoAuto <- NULL
+
+
+###-----------------------------------------------------------------------------------------------------
+df.Br101 <- subset(df,BR=='101')
+df.Br232 <- subset(df,BR=='232')
+
+write.csv(df.Br101,"./data/BR101/RNNLiteral.csv", row.names = FALSE)
+write.csv(df.Br232,"./data/BR232/RNNLiteral.csv", row.names = FALSE)
+
+# Procura em algum Km em determinada hora um acidente e conta-o
+myCont <- function (km,hr)  {
+  y = 0
+  for(i in 1:nrow(df.Br101)){
+    if (df.Br101[i,13] == km && df.Br101[i,12] == hr) {
+      y = y + df.Br101[i,10]
+    }
+  }
+  return(y)
+}
+
+### função para crirar uma matriz de n rows e m columns
+criarMatriz <- function(lin,col) {
+  my.matrix <- c()
+  for(i in 1:lin){
+    my.matrix <- rbind(my.matrix, rep(0,col))
+  }  
+  return(my.matrix)
+}
+
+#nrow(df.Br101)
+
+criarMatrizFinal <- function(lin,col){
+  r <- criarMatriz(lin,col)
+  for(i in 1:nrow(df.Br101)){
+    r[df.Br101[i,8]+1,df.Br101[i,9]+1] <- r[df.Br101[i,8]+1,df.Br101[i,9]+1] + df.Br101[i,7]
+  }
+#  for(i in 0:(lin - 1)){
+#    for(j in 0:(col - 1)){
+#      r[i,j] <- myCont(i,j)
+#    }
+#  }
+  return(r)
+}
+
+
+l = 24
+c = max(df.Br101$KMArredondado)+1
+
+
+s = criarMatrizFinal(l,c)
+
+### Converte para data frame & Alterar o no me da linha e coluna
+dfs <- as.data.frame(s)
+colnames(dfs) = c(0:(c -1))
+row.names(dfs) = c(0:(l -1))
+
+write.csv(dfs,"./data/BR101/MatrizMortos.csv", row.names = FALSE)
+
+################################################################################
+############################             #######################################
+###########################     BR 232      ####################################
+############################             #######################################
+################################################################################
+
+## calcula quantidade de mortos o atributo Gravidade------------------------------------------------
+#df <- read.csv("./data/LimpoCheiroso.csv") ## carrega o data frame
+
+#### limpando dados
+#df$Binario <- NULL
+#df$Gravidade <- NULL
+#df$DiaDaSemana <- NULL
+#df$Municipio <- NULL
+##df$TotFerVivos <- NULL
+#df$TipoAuto <- NULL
+
+
+###-----------------------------------------------------------------------------------------------------
+#df.Br101 <- subset(df,BR=='101')
+#df.Br232 <- subset(df,BR=='232')
+
+#write.csv(df.Br101,"./data/BR101/RNNLiteral.csv", row.names = FALSE)
+#write.csv(df.Br232,"./data/BR232/RNNLiteral.csv", row.names = FALSE)
+
+# Procura em algum Km em determinada hora um acidente e conta-o
+myCont <- function (km,hr)  {
+  y = 0
+  for(i in 1:nrow(df.Br232)){
+    if (df.Br232[i,13] == km && df.Br232[i,12] == hr) {
+      y = y + df.Br232[i,10]
+    }
+  }
+  return(y)
+}
+
+### função para crirar uma matriz de n rows e m columns
+criarMatriz <- function(lin,col) {
+  my.matrix <- c()
+  for(i in 1:lin){
+    my.matrix <- rbind(my.matrix, rep(0,col))
+  }  
+  return(my.matrix)
+}
+
+### Cria uma matriz genérica 2 X 2
+criarMatrizFinal <- function(lin,col){
+  r <- criarMatriz(lin,col)
+  for(i in 1:nrow(df.Br232)){
+    r[df.Br232[i,8]+1,df.Br232[i,9]+1] <- r[df.Br232[i,8]+1,df.Br232[i,9]+1] + df.Br232[i,7]
+  }
+  return(r)
+}
+
+### Define as dimensões da matriz de Mortos e cria
+l = 24
+c = max(df.Br232$KMArredondado)+1
+mm232 = criarMatrizFinal(l,c)
+
+### Converte para data frame & Alterar o no me da linha e coluna
+df232 <- as.data.frame(mm232)
+colnames(df232) = c(0:(c -1))
+row.names(df232) = c(0:(l -1))
+
+write.csv(df232,"./data/BR232/MatrizMortos.csv", row.names = FALSE)
+
+################################################################################
+############################             #######################################
+###########################     BR 104      ####################################
+############################             #######################################
+################################################################################
+
+## calcula quantidade de mortos o atributo Gravidade------------------------------------------------
+#df <- read.csv("./data/LimpoCheiroso.csv") ## carrega o data frame
+
+###-----------------------------------------------------------------------------------------------------
+#df.Br101 <- subset(df,BR=='101')
+#df.Br232 <- subset(df,BR=='232')
+df.Br104 <- subset(df,BR=='104')
+
+#write.csv(df.Br101,"./data/BR101/RNNLiteral.csv", row.names = FALSE)
+#write.csv(df.Br232,"./data/BR232/RNNLiteral.csv", row.names = FALSE)
+
+# Procura em algum Km em determinada hora um acidente e conta-o
+myCont <- function (km,hr)  {
+  y = 0
+  for(i in 1:nrow(df.Br232)){
+    if (df.Br232[i,13] == km && df.Br232[i,12] == hr) {
+      y = y + df.Br232[i,10]
+    }
+  }
+  return(y)
+}
+
+### função para crirar uma matriz de n rows e m columns
+criarMatriz <- function(lin,col) {
+  my.matrix <- c()
+  for(i in 1:lin){
+    my.matrix <- rbind(my.matrix, rep(0,col))
+  }  
+  return(my.matrix)
+}
+
+### Cria uma matriz genérica 2 X 2
+criarMatrizFinal <- function(lin,col){
+  r <- criarMatriz(lin,col)
+  for(i in 1:nrow(df.Br232)){
+    r[df.Br232[i,8]+1,df.Br232[i,9]+1] <- r[df.Br232[i,8]+1,df.Br232[i,9]+1] + df.Br232[i,7]
+  }
+  return(r)
+}
+
+### Define as dimensões da matriz de Mortos e cria
+l = 24
+c = max(df.Br232$KMArredondado)+1
+mm232 = criarMatrizFinal(l,c)
+
+### Converte para data frame & Alterar o no me da linha e coluna
+df232 <- as.data.frame(mm232)
+colnames(df232) = c(0:(c -1))
+row.names(df232) = c(0:(l -1))
+
+write.csv(df232,"./data/BR232/MatrizMortos.csv", row.names = FALSE)
+
